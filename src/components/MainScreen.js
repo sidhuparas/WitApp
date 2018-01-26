64f283656cf8af74f651dca1c7667d014fc6bb74
Paras Sidhu <sidhuparas1234@gmail.com>
2018-01-26 21:20:45 +0530

    Now Functional
    Setup Axios to make POST requests

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,29 +1,67 @@
 import React, { Component } from 'react';
-import {View, Text} from 'react-native';
+import { View, Text, NetInfo} from 'react-native';
+import axios from 'axios';
 import {Body, Button, Header, Content, Icon, Input, Item, Left, Right, Title, Toast, Container} from 'native-base';
+import Spinner from './Spinner';
 
 class MainScreen extends Component {
+  state = { text: '', intent: [], result: '', loading: false };
 
-onSubmit(){
+
+getit(){
+  const { text, result, loading } = this.state;
+  this.setState({  loading: true, intent:'' });
+  const formData = new FormData();
+  formData.append('comment', text);
+
+  axios.post('https://app.assassinate72.hasura-app.io/wit', formData)
+  .then(response =>
+    this.setState({ intent: 'Intent is ' + response.data.intent[0].value, loading: false })
+  )
+  .catch(error =>
+    this.setState({ intent: 'An error occurred while processing your request. ' + error.message, loading: false })
+  );
+}
+
+renderButton() {
+    if (this.state.loading) {
+      return <Spinner size="large" />;
+    }
+
+    return (
+    <Button iconLeft light
+    style={styles.buttonStyle}
+    onPress={this.getit.bind(this)}
+    >
+
+    <Text style={{ color: '#fff', fontSize:16 }}>Submit</Text>
+    </Button>
+  );
+}
+
+onSubmit() {
   Toast.show({
-    text: 'Wrong password!',
+    text: 'Done',
     position: 'bottom',
-    buttonText: 'Okay'});
+    buttonText: 'OK' });
 }
 
   render(){
     return (
       <View>
       <Item>
-        <Input placeholder="Enter your query..." />
+        <Input
+          placeholder="Enter your query..."
+          onChangeText={text => this.setState({ text })}
+        />
       </Item>
 
       <View style={styles.containerStyle}>
-        <Button iconLeft light
-        style={styles.buttonStyle}
-        onPress={this.onSubmit.bind(this)}>
-        <Text style={{color:'#fff'}}>Submit</Text>
-        </Button>
+        {this.renderButton()}
+
+      </View>
+      <View>
+        <Text style={styles.resultStyle}> {this.state.intent} </Text>
       </View>
       </View>
     );
@@ -39,7 +77,7 @@ const styles={
   },
 
   buttonStyle:{
-      backgroundColor: '#007aff',
+      backgroundColor: 'green',
       padding: 20,
       marginTop: 10
   },
@@ -48,6 +86,12 @@ const styles={
     marginLeft:10,
     marginRight:10,
     marginBottom:5
+  },
+
+  resultStyle:{
+    color: 'green',
+    fontSize:16,
+    marginTop:20
   }
 }
 
